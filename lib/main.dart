c5a333393a744d1ec50ca1de3f0eb00103cfc785
박지민 <parkjimin@bagjimin-ui-MacBookAir.local>
2024-07-04 05:21:22 +0900

    홈화면 슬라이더위젯 추가

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/screen/home_screen.dart';
+import 'package:flutter_application/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '넷플릭스',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          hintColor: Colors.white),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              Container(child: Container(child: Center(child: Text('search'),),),),
+              Container(child: Container(child: Center(child: Text('save'),),),),
+              Container(child: Container(child: Center(child: Text('more'),),),),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
